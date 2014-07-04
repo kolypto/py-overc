e97c98ba01629cd501e3d473edc6f4085eb3cf46
Mark Vartanyan <kolypto@gmail.com>
2014-07-04 14:16:28 +0200

    docker: fix

--- misc/docker/overc-server/run.sh
+++ misc/docker/overc-server/run.sh
@@ -21,8 +21,8 @@ export OVERC_UI_AUTH=${OVERC_UI_AUTH:-""}
 j2 /root/conf/nginx-site.conf > /etc/nginx/sites-enabled/overc.conf
 
 # Passwords
-htpasswd -cbd /etc/nginx/htpasswd-api ${OVERC_API_AUTH/:/ }
-htpasswd -cbd /etc/nginx/htpasswd-ui ${OVERC_UI_AUTH/:/ }
+[ ! -z "$OVERC_API_AUTH" ] && htpasswd -cbd /etc/nginx/htpasswd-api ${OVERC_API_AUTH/:/ }
+[ ! -z "$OVERC_UI_AUTH" ]  && htpasswd -cbd /etc/nginx/htpasswd-ui  ${OVERC_UI_AUTH/:/ }
 
 # Logging
 rm -f /var/run/nginx.pid
