6375a74bcb77613b742e76fb120c1652df7ecafd
Mark Vartanyan <kolypto@gmail.com>
2014-07-11 15:38:25 +0200

    v1.0.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     # http://pythonhosted.org/setuptools/setuptools.html
     name='overc',
-    version='1.0.8-2',
+    version='1.0.9-0',
     author='Mark Vartanyan',
     author_email='kolypto@gmail.com',
 
